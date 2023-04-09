b827713a208723cb4d674ba8fd6e2359599a2f9c
Andrzej Piotrowski <andrzej.piotrowski@avenga.com>
2023-04-09 21:06:07 +0200

    npm update

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '611c9eabe47a34145aa8');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '469fc7b59742c4509ee1');
