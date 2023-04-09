bd5a7b383dee3d32b98cab44de919a3d34e9f378
Andrzej Piotrowski <andrzej.piotrowski@avenga.com>
2023-04-09 20:58:46 +0200

    packages update

--- README.md
+++
@@ -1,2 +0,0 @@
-# products-slider-block
-Products Slider Block for WooCommerce
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e717e5d7dc3f2b12140c');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '611c9eabe47a34145aa8');
