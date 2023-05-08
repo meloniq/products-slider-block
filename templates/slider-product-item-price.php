ce42f17ef580c6ae3f7cf16dcc6f119c95c334b5
Andrzej Piotrowski <andrzej.piotrowski@avenga.com>
2023-05-08 13:51:21 +0200

    escape price and thumbnail html code with wp_kses_post()

--- templates/slider-product-item-price.php
+++ templates/slider-product-item-price.php
@@ -4,6 +4,6 @@ $price = $product->get_price_html();
 
 <div class="psb-slider-product-item-price-wrap">
 	<span class="price">
-		<?php echo $price; ?>
+		<?php echo wp_kses_post( $price ); ?>
 	</span>
 </div>
--- templates/slider-product-item-thumbnail.php
+++ templates/slider-product-item-thumbnail.php
@@ -4,6 +4,6 @@ $image = get_the_post_thumbnail( $post );
 
 <div class="psb-slider-product-item-thumbnail-wrap">
 	<a href="<?php echo esc_url( $url ); ?>">
-		<?php echo $image; ?>
+		<?php echo wp_kses_post( $image ); ?>
 	</a>
 </div>
