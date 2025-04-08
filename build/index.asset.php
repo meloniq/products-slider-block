db56dea78af0491a614a0c21c10ad901b3e1b749
meloniq <meloniq@gmail.com>
2025-04-08 21:08:04 +0300

    update builds

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '8d24a0019438e3208e88');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '8cfb66e2f17dc03cd97a');
